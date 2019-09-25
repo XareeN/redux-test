3d9b697b766bcf0f3295fa4f37973d40d0054e2b
XareeN <xareen@interia.eu>
2019-09-25 18:10:40 +0200

    Set up boilerplate for redux..

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react'
-import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
 
 import Posts from './components/Posts';
 import Postform from './components/Postform';
 
+import store from './store';
+
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Postform/>
-        <hr/>
-        <Posts/>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <Postform/>
+          <hr/>
+          <Posts/>
+        </div>
+      </Provider>
     )
   }
 }
